1f72cb50fdaf1f19fc932d65dcae2f695b3f82a2
storm <snowcra5h@icloud.command>
2024-02-19 17:12:54 -0700

    add depends also add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "ruzz")]
+struct Opt {
+    #[structopt(short, long, default_value = "http://localhost")]
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
+    println!("Target URL: {}", opt.url);
+
 }
